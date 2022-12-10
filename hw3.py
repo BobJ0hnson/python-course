091ca764137f2f367138ab736d556f09dfff2fb0
BobJ0hnson <119344933+BobJ0hnson@users.noreply.github.com>
2022-12-10 18:16:15 +0200

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,17 +1,20 @@
 
-
+#1#
 string='I love Python '
 print(string * 42)
 
+#2#
 age_in_month = 33 * 12
 age_in_years = int(age_in_month / 12)
 my_age = f"My name is Yaroslav, I'm {age_in_years} years old"
 print(my_age)
 
+#3#
 i = 1
 list1 = {4,3,2,1,0}
 print(i == list1)
 
+#4#
 a = 2
 b = 5
 c = 6
