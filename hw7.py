b414ac6b6a7e6f462aaff0276483383a2266317a
BobJ0hnson <119344933+BobJ0hnson@users.noreply.github.com>
2023-01-05 11:38:30 +0200

    Add files via upload

--- hw7.py
+++ hw7.py
@@ -3,8 +3,10 @@ while True:
     user_input = input('Enter command: ')   # format command name phone
     split_input = user_input.split()
     command = split_input[0]
-    name = split_input[1]
-    phone = split_input[2]
+    if len(split_input) > 1:
+        name = split_input[1]
+    if len(split_input) > 2:
+        phone = split_input[2]
 
     if command == 'add':
         if phonebook.get(name) is None:
